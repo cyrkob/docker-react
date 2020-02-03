75f29823978a3520cf7a5708a267898075d753f4
ksm <kmanikas@gmail.com>
2020-02-02 19:08:55 -0500

    Denethor

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Frodo Baggins 
+          Custodian Denethor 
         </p>
       </div>
     );
